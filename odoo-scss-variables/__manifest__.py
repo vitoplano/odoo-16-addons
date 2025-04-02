3ea34dc3546cdc6ddd334232c498170a28af68e7
Vito Plano <vito.plano@martel-innovate.com>
2025-04-02 15:49:03 +0200

    fix variabile addons2

---
+++ odoo-scss-variables/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Odoo SCSS Global Variables',
+    'version': '16.0.1.0.0',
+    'category': 'Technical',
+    'license': 'MIT',
+    'summary': 'Global SCSS variables for Odoo',
+    'description': 'Provides global SCSS variables to be used across Odoo modules',
+    'author': 'Your Company Name',
+    'website': 'https://yourcompany.com',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            '/odoo-scss-variables/static/src/scss/global_variables.scss',
+        ],
+        'web.assets_frontend': [
+            '/odoo-scss-variables/static/src/scss/global_variables.scss',
+        ],
+    },
+    'installable': True,
+    'auto_install': True,
+}
